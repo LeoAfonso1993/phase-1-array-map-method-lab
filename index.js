2cbe3865f08125d959918fa37d260aaa0d2f2281
Leandro Afonso <leandro.afonso@hotmail.com>
2022-04-03 03:23:18 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -11,6 +11,63 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
+/*const titleCased = () => {
   return tutorials
+}*/
+
+/*function wordToUpper(array){
+  for (i = 0; i < array.length; i++){
+    for (word of array[i]){
+      word[0].toUpperCase()
+      console.log(array[i])
+    }
+  }
 }
+
+console.log(wordToUpper(tutorials))*/
+
+
+/*const titleCased = function(array) {
+   const sent = array.map(function(title) {
+     const words = title.split(" ")
+     for (let i = 0; i < words.length; i ++) {
+       words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1)
+     }
+     return title = words.join(" ")
+   }
+  )
+}*/
+
+/*const titleCased = tutorials.map(function(title){
+  const breakdown = title.split(" ")
+  for (let i = 0; i < breakdown.length; i++) {
+    breakdown[i] = breakdown[i].charAt(0).toUpperCase() + breakdown[i].slice(1)
+  }
+  return title = breakdown.join(" ")
+})
+*/
+
+/*function iter(array){
+  for (element of array){
+    const str = element.split(" ")
+    for(word of str){
+      const str2 = word.toString().charAt(0).toUpperCase() + word.slice(1)
+      const jun = str2
+      console.log(jun)
+    }
+  } 
+}*/
+
+
+function titleCased(){
+  const applyConc = tutorials.map(sentence => {
+    const newArr = sentence.split(" ").map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(" ");
+
+    return newArr; 
+  })
+  return applyConc;
+}
+
+console.log(titleCased(tutorials))
